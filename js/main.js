630059f6ee522415ae55742bb6ad4bd5fc4a7042
taisei <taisei.radio.1207@gmail.com>
2024-07-23 16:23:03 +0900

    question js 作成

--- js/main.js
+++ js/main.js
@@ -10,16 +10,9 @@ if(window_default_w>breakpoint){
 
 // footer sp
 $(function(){
-  if(!sizeSp){
-    $(".js-footerPa_pc").click(function(){
-      $(this).toggleClass("active");
-      $(".js-footerCh_pc").slideToggle(400);
-    });
-  }
-  if(sizeSp){
-    $(".js-footerPa").click(function(){
-      $(this).toggleClass("active");
-      $(this).next(".js-footerCh").slideToggle(400);
-    });
-  }
+  $(".js-qanda-btn").click(function(){
+    $(this).toggleClass("active");
+    $(this).prev().toggleClass("active");
+    $(this).parent().next().slideToggle(400);
+  });
 });
